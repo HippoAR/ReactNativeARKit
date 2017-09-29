6a03795f5c1a50d6ba0dc3c3f4a0d50619ed824b
qftgtr <qft.gtr@gmail.com>
2017-09-29 15:16:44 +0800

    react-native-arkit@0.2.3

--- index.ios.js
+++ index.ios.js
@@ -19,64 +19,67 @@ export default class ReactNativeARKit extends Component {
           onPlaneUpdate={console.log} // event listener for plane update
         >
           <ARKit.Box
-            pos={{ x: 0, y: 0, z: 0 }}
+            position={{ x: 0, y: 0, z: 0 }}
             shape={{ width: 0.1, height: 0.1, length: 0.1, chamfer: 0.01 }}
             material={{ diffuse }}
           />
           <ARKit.Sphere
-            pos={{ x: 0.2, y: 0, z: 0 }}
+            position={{ x: 0.2, y: 0, z: 0 }}
             shape={{ radius: 0.05, }}
             material={{ diffuse }}
           />
           <ARKit.Cylinder
-            pos={{ x: 0.4, y: 0, z: 0 }}
+            position={{ x: 0.4, y: 0, z: 0 }}
             shape={{ radius: 0.05, height: 0.1 }}
             material={{ diffuse }}
           />
           <ARKit.Cone
-            pos={{ x: 0, y: 0.2, z: 0 }}
+            position={{ x: 0, y: 0.2, z: 0 }}
             shape={{ topR: 0, bottomR: 0.05, height: 0.1 }}
             material={{ diffuse }}
           />
           <ARKit.Pyramid
-            pos={{ x: 0.2, y: 0.15, z: 0 }}
+            position={{ x: 0.2, y: 0.15, z: 0 }}
             shape={{ width: 0.1, height: 0.1, length: 0.1 }}
             material={{ diffuse }}
           />
           <ARKit.Tube
-            pos={{ x: 0.4, y: 0.2, z: 0 }}
+            position={{ x: 0.4, y: 0.2, z: 0 }}
             shape={{ innerR: 0.03, outerR: 0.05, height: 0.1 }}
             material={{ diffuse }}
           />
           <ARKit.Torus
-            pos={{ x: 0, y: 0.4, z: 0 }}
+            position={{ x: 0, y: 0.4, z: 0 }}
             shape={{ ringR: 0.06, pipeR: 0.02 }}
             material={{ diffuse }}
           />
           <ARKit.Capsule
-            pos={{ x: 0.2, y: 0.4, z: 0 }}
+            position={{ x: 0.2, y: 0.4, z: 0 }}
             shape={{ capR: 0.02, height: 0.06 }}
             material={{ diffuse }}
           />
           <ARKit.Plane
-            pos={{ x: 0.4, y: 0.4, z: 0 }}
+            position={{ x: 0.4, y: 0.4, z: 0 }}
             shape={{ width: 0.1, height: 0.1 }}
             material={{ diffuse }}
           />
           <ARKit.Text
             text="ARKit is Cool!"
-            pos={{ x: 0.2, y: 0.6, z: 0 }}
+            position={{ x: 0.2, y: 0.6, z: 0 }}
+            eulerAngles={{ y: 3.14 / 6 }}
             font={{ size: 0.15, depth: 0.05 }}
             material={{ diffuse }}
           />
           <ARKit.Text
             text="made by react-native-arkit"
-            pos={{ x: 0, y: 0, z: 0, frame: 'FrontOfCamera' }}
+            frame="FrontOfCamera"
+            position={{ x: 0, y: 0, z: 0 }}
+
             font={{ size: 0.01, depth: 0.002 }}
             material={{ diffuse: 'blue' }}
           />
           <ARKit.Model
-            pos={{ x: -0.2, y: 0, z: 0, frame: 'Local' }}
+            position={{ x: -0.2, y: 0, z: 0 }}
             model={{ file: 'art.scnassets/ship.scn', scale: 0.03 }}
           />
         </ARKit>
